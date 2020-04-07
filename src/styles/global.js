bba39777aac2e534daf9f55e887805168f4b9c80
Rafael Brito <43880669+rafaelconcept@users.noreply.github.com>
2020-04-07 19:49:18 -0300

    fazendo um teste
    testando pull

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,7 +4,7 @@ export default createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
 
     * {
-        margin: 0;
+        margin: 1;
         padding: 0;
         outline: 0;
         box-sizing: border-box;
