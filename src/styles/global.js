5bc228db9541adbb62840d5c8210bf5dbc9fdff1
Rafael Brito <rafaelconcept@hotmail.com>
2020-04-07 20:50:42 -0300

    adicionado alguns css

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,14 +1,29 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
 
     * {
-        margin: 1;
+        margin: 0;
         padding: 0;
         outline: 0;
         box-sizing: border-box;
 
     }
 
+    html, body, #root {
+        height: 100%;
+    }
+
+    body {
+        font-size: 14px 'Roboto', sans-serif;
+        background: #ecf1f8;
+        color: #333;
+        -webkit-font-smoothing: antialiased !important;
+
+    }
+
+    ul {
+        list-style: none;
+    }
 `;
