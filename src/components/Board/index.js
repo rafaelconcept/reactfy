6e7c61dd9d43444a6a696653d3aa0682c3561f5b
Rafael Brito <rafaelconcept@hotmail.com>
2020-04-08 01:41:08 -0300

    drop funcionando

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import produce from 'immer';
 import { Container } from './styles';
+import List from '../List';
+import { loadLists } from '../../services/api';
+import BoardContext from './context';
+
+const data = loadLists();
+
 
 export default function Board() {
-  return (
-    <Container>
+  const [lists, setList] = useState(data);
+  function move(fromList, toList, from, to){
 
-    </Container>
+    setList(produce(lists, draft => {
+      
+      const dragged = draft[fromList].cards[from]
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+
+    }))
+
+  }
+
+  return (
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}     
+      </Container>
+    </BoardContext.Provider>
   );
 }
